f161a7b353c22fe9a6d934b7ea5dd65e719f9c5d
DELL-PC\Andry Zirka <zirkaandry@gmail.com>
2017-11-09 12:37:48 +0200

    Серьезные правки по стилях

--- dist/libs/LocalMarker.js
+++ dist/libs/LocalMarker.js
@@ -1,7 +1,21 @@
-window.onload = function(){
-	// var body = document.getElementsByTagName("body");
-	var body = $("body");
+window.onload = function () {
+    var a = window.location.toString();
+        console.log("a =" + a);
 
-	body.append('<div id="marker"></div>');
 
-}
+    var body = document.body;
+    var divMarker =  document.createElement('div');
+        divMarker.id = 'marker';
+    divMarker.style.cssText = 'position: fixed;top: 50%;left: 0;height: 0;width: 0;border-top: 40px solid transparent;border-bottom: 40px solid transparent;border-left: 80px solid #f00;font-family: Arial, sans-serif; font-weight: 700;color: #fff; line-height: 80px;z-index: 2;';
+
+    var after = document.createElement('div');
+        after.style.cssText ='position:absolute;transform:translate(-150%, -50%)';
+        after.innerHTML = 'Layout';
+
+
+
+    body.appendChild(divMarker);
+    divMarker.appendChild(after);
+
+
+};
